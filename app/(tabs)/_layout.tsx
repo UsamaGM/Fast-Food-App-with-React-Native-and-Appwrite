07a25cdde4bee75da928a2645190eb3d83adbf60
UsamaGM <usamapk7861@gmail.com>
2025-07-14 02:29:28 +0500

    Add bottom tab navigation with custom styling

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,11 +1,93 @@
+import { images } from "@/contants";
 import useAuthStore from "@/store/auth.store";
-import { Redirect, Slot } from "expo-router";
+import { TabBarIconProps } from "@/type";
+import { Redirect, Tabs } from "expo-router";
+import { Image, View, Text } from "react-native";
+import cn from "clsx";
+
+function TabBarIcon({ focused, icon, title }: TabBarIconProps) {
+  return (
+    <View className="tab-icon">
+      <Image
+        source={icon}
+        className="size-7"
+        resizeMode="contain"
+        tintColor={focused ? "#fe8c00" : "#5d5f6d"}
+      />
+      <Text
+        className={cn(
+          "text-sm font-bold",
+          focused ? "text-primary" : "text-gray-200",
+        )}
+      >
+        {title}
+      </Text>
+    </View>
+  );
+}
 
 function TabsLayout() {
   const { isAuthenticated } = useAuthStore();
 
   if (!isAuthenticated) return <Redirect href="/sign-in" />;
-  return <Slot />;
+  return (
+    <Tabs
+      screenOptions={{
+        headerShown: false,
+        tabBarShowLabel: false,
+        tabBarStyle: {
+          borderRadius: 50,
+          marginHorizontal: 20,
+          height: "8%",
+          position: "absolute",
+          bottom: "2%",
+          backgroundColor: "white",
+          shadowColor: "#1a1a1a",
+          shadowOffset: { width: 0, height: 2 },
+          shadowOpacity: 0.1,
+          shadowRadius: 4,
+          elevation: 5,
+        },
+      }}
+    >
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Home",
+          tabBarIcon: ({ focused }) => (
+            <TabBarIcon focused={focused} icon={images.home} title="Home" />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="search"
+        options={{
+          title: "Search",
+          tabBarIcon: ({ focused }) => (
+            <TabBarIcon focused={focused} icon={images.search} title="Search" />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="cart"
+        options={{
+          title: "Cart",
+          tabBarIcon: ({ focused }) => (
+            <TabBarIcon focused={focused} icon={images.bag} title="Cart" />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="profile"
+        options={{
+          title: "Profile",
+          tabBarIcon: ({ focused }) => (
+            <TabBarIcon focused={focused} icon={images.user} title="Profile" />
+          ),
+        }}
+      />
+    </Tabs>
+  );
 }
 
 export default TabsLayout;
--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,6 +1,5 @@
 import CartButton from "@/components/CartButton";
 import { images, offers } from "@/contants";
-import useAuthStore from "@/store/auth.store";
 import { Fragment } from "react";
 import {
   FlatList,
@@ -13,10 +12,6 @@ import {
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
-  const { user } = useAuthStore();
-
-  console.log(JSON.stringify(user, null, 2));
-
   return (
     <SafeAreaView className="flex-1 bg-white">
       <FlatList
